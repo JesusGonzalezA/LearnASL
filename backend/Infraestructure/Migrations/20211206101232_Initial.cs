e30aa52f7e2e013a57d47285aec874ab5e9ae17a
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-12-06 12:23:19 +0200

    Add database and migration

---
+++ backend/Infraestructure/Migrations/20211206101232_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infraestructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: false),
+                    Password = table.Column<string>(type: "varchar(200)", unicode: false, maxLength: 200, nullable: false),
+                    ConfirmedEmail = table.Column<bool>(type: "bit", nullable: false),
+                    TokenPasswordRecovery = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    TokenEmailConfirmation = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
