521ba4394da6c0b1f8ad35b17ccbeccd0a130ef7
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-12-06 12:25:22 +0200

    Implement unit of work pattern

---
+++ backend/Infraestructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Core.Interfaces;
+using Infraestructure.Data;
+
+namespace Infraestructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private readonly IUserRepository _userRepository;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository UserRepository => _userRepository ?? new UserRepository(_context);
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
