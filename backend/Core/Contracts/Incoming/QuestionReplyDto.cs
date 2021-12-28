576301771924bc53a81dcda3b0f411b0799b17ae
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-12-28 11:01:02 +0100

    Add dto to reply

--- backend/Core/Contracts/Incoming/QuestionReplyDto.cs
+++ backend/Core/Contracts/Incoming/QuestionReplyDto.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Enums;
+using Core.ValidationAttributes;
+using Microsoft.AspNetCore.Http;
+
 namespace Core.Contracts.Incoming
 {
     public class QuestionReplyDto
     {
-        public QuestionReplyDto()
-        {
-        }
+        [Required]
+        public TestType TestType { get; set; }
+
+        public string? UserAnswer { get; set; }
+
+        [DataType(DataType.Upload)]
+        [MaxFileSize(5 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".mp4" })]
+        public IFormFile? VideoUser { get; set; }
     }
 }
