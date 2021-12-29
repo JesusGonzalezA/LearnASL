998425c18367a917058c1034968cd1780b1535a8
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-12-29 14:11:29 +0100

    Test TestService and filter extension method

---
+++ backend/UnitTests/Core/Extensions/IEnumerableOfTestEntity_Filter_TestQueryFilter.test.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Entities.Tests;
+using Core.Enums;
+using Core.Extensions;
+using Core.Interfaces;
+using Core.Options;
+using Core.QueryFilters;
+using Core.Services;
+using Microsoft.Extensions.Options;
+using Tests.Mocks;
+using Xunit;
+
+namespace Tests.Core.Extensions
+{
+    public partial class TestIEnumerableOfTestEntity_Filter_TestQueryFilter
+    {
+        [Fact]
+        public async Task Extensions_FilterTests_FiltersCorrectly()
+        {
+            TestService testService = CreateTestService();
+            Guid userId = await AddUser();
+            TestQueryFilter filters = new TestQueryFilter() { UserId = userId };
+
+            IEnumerable<TestEntity> testEntities = new List<TestEntity>()
+            {
+                await CreateTestEntity(userId, Difficulty.EASY, TestType.Mimic),
+                await CreateTestEntity(userId, Difficulty.MEDIUM, TestType.Mimic_Error),
+                await CreateTestEntity(userId, Difficulty.HARD, TestType.QA),
+                await CreateTestEntity(userId, Difficulty.EASY, TestType.QA_Error),
+                await CreateTestEntity(userId, Difficulty.MEDIUM, TestType.OptionVideoToWord),
+                await CreateTestEntity(userId, Difficulty.HARD, TestType.OptionVideoToWord_Error),
+                await CreateTestEntity(userId, Difficulty.EASY, TestType.OptionWordToVideo),
+                await CreateTestEntity(userId, Difficulty.MEDIUM, TestType.OptionWordToVideo_Error)
+            };
+
+            IEnumerable<TestEntity> allTests = await _unitOfWork.TestRepository.GetAll();
+            Assert.Empty(allTests);
+
+            // Filter by user
+            foreach (TestEntity test in testEntities)
+            {
+                await testService.AddTest(test);
+            }
+
+            allTests = await _unitOfWork.TestRepository.GetAll();
+            allTests = allTests.Filter(filters);
+            Assert.Equal(testEntities, allTests);
+
+            // Filter by type
+            filters.TestType = TestType.Mimic;
+            IEnumerable<TestEntity> mimicTests = testEntities
+                .Where(test => test.TestType == filters.TestType)
+                .ToList();
+
+            allTests = await _unitOfWork.TestRepository.GetAll();
+            allTests = allTests.Filter(filters);
+            Assert.Equal(allTests, mimicTests);
+            filters.TestType = null;
+
+            // Filter by difficulty
+            filters.Difficulty = Difficulty.EASY;
+            IEnumerable<TestEntity> easyTests = testEntities
+                .Where(test => test.Difficulty == Difficulty.EASY)
+                .ToList();
+
+            allTests = await _unitOfWork.TestRepository.GetAll();
+            allTests = allTests.Filter(filters);
+            Assert.Equal(allTests, easyTests);
+        }
+    }
+
+    public partial class TestIEnumerableOfTestEntity_Filter_TestQueryFilter
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly PaginationOptions _paginationOptions;
+
+        public TestIEnumerableOfTestEntity_Filter_TestQueryFilter()
+        {
+            _unitOfWork = MockUnitOfWork.GetMockUnitOfWork();
+            _paginationOptions = new PaginationOptions()
+            {
+                DefaultPageNumber = 1,
+                DefaultPageSize = 10,
+                MaximumPageSize = 20
+            };
+        }
+
+        private TestService CreateTestService()
+        {
+            IQuestionService questionService = new QuestionService(_unitOfWork);
+            IOptions<PaginationOptions> paginationOptions = Options.Create(_paginationOptions);
+            return new TestService(_unitOfWork, questionService, paginationOptions);
+        }
+
+        private async Task<Guid> AddUser()
+        {
+            UserService userService = CreateUserService();
+            UserEntity userEntity = new UserEntity("test@mail.com", "test");
+
+            return await userService.AddUser(userEntity);
+        }
+
+        private UserService CreateUserService()
+        {
+            return new UserService(_unitOfWork);
+        }
+
+        private async Task<TestEntity> CreateTestEntity
+        (
+            Guid userId,
+            Difficulty difficulty = Difficulty.EASY,
+            TestType testType = TestType.Mimic
+        )
+        {
+            TestEntity testEntity = new TestEntity()
+            {
+                UserId = userId,
+                Difficulty = difficulty,
+                TestType = testType,
+                User = await CreateUserService().GetUser(userId)
+            };
+
+            return testEntity;
+        }
+    }
+}
--- backend/UnitTests/Core/Services/QuestionService.test.cs
+++ backend/UnitTests/Core/Services/QuestionService.test.cs
@@ -1,10 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Entities.Tests;
+using Core.Enums;
+using Core.Interfaces;
+using Core.Options;
+using Core.Services;
+using Infraestructure.Interfaces;
+using Infraestructure.Services;
+using Microsoft.Extensions.Options;
+using Tests.Mocks;
+using Xunit;
+
 namespace Tests.Core.Services
 {
-    public class QuestionService
+    public partial class TestQuestionService
     {
-        public QuestionService()
+        [Theory]
+        [MemberData(nameof(AllTestTypes))]
+        public async Task QuestionService_AddQuestions_AddsCorrectly(TestType testType)
         {
+            BaseQuestionEntity question = _questionGeneratorService.CreateQuestion(testType, Difficulty.EASY, Guid.Empty);
+        }
+
+    }
+
+    public partial class TestQuestionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly PaginationOptions _paginationOptions;
+        private readonly IQuestionGeneratorService _questionGeneratorService;
+
+        public TestQuestionService()
+        {
+            _unitOfWork = MockUnitOfWork.GetMockUnitOfWork();
+            _paginationOptions = new PaginationOptions()
+            {
+                DefaultPageNumber = 1,
+                DefaultPageSize = 10,
+                MaximumPageSize = 20
+            };
+            _questionGeneratorService = new QuestionGeneratorService();
+        }
+
+        public static IEnumerable<object[]> AllTestTypes()
+        {
+            foreach (var number in Enum.GetValues(typeof(TestType)))
+            {
+                yield return new object[] { number };
+            }
+        }
+
+        private TestService CreateTestService()
+        {
+            IQuestionService questionService = new QuestionService(_unitOfWork);
+            IOptions<PaginationOptions> paginationOptions = Options.Create(_paginationOptions);
+            return new TestService(_unitOfWork, questionService, paginationOptions);
+        }
+
+        private UserService CreateUserService()
+        {
+            return new UserService(_unitOfWork);
+        }
+
+        private async Task<Guid> AddUser()
+        {
+            UserService userService = CreateUserService();
+            UserEntity userEntity = new UserEntity("test@mail.com", "test");
+
+            return await userService.AddUser(userEntity);
+        }
+
+        private async Task<TestEntity> CreateTestEntity
+        (
+            Guid userId,
+            Difficulty difficulty = Difficulty.EASY,
+            TestType testType = TestType.Mimic
+        )
+        {
+            TestEntity testEntity = new TestEntity()
+            {
+                UserId = userId,
+                Difficulty = difficulty,
+                TestType = testType,
+                User = await CreateUserService().GetUser(userId)
+            };
+
+            return testEntity;
         }
     }
 }
