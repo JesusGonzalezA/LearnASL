59cdb8106d6fd85bb6f16dfcdd286c00901f2bdc
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2022-02-02 16:56:38 +0200

    Change response type

--- backend/Api/Controllers/StatsController.cs
+++ backend/Api/Controllers/StatsController.cs
@@ -30,7 +30,7 @@ namespace Api.Controllers
         }
 
         [HttpGet("use-of-the-app")]
-        [ProducesResponseType(typeof(IEnumerable<int>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.Conflict)]
         public IActionResult GetUseOfTheApp([FromQuery] StatsQueryFilterUseOfTheAppDto filtersDto)
         {
@@ -39,31 +39,31 @@ namespace Api.Controllers
 
             IEnumerable<int> monthlyUseOfTheAppByUser = _statsService.GetMonthlyUseOfTheAppByUser(filters);
 
-            return Ok(monthlyUseOfTheAppByUser);
+            return Ok(new { stat = monthlyUseOfTheAppByUser });
         }
 
         [HttpGet("best-streak")]
-        [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.Conflict)]
         public IActionResult GetBestStreak()
         {
             int bestStreak = _statsService.GetBestStreak(GuidOfCurrentUser);
 
-            return Ok(bestStreak);
+            return Ok(new { stat = bestStreak });
         }
 
         [HttpGet("current-streak")]
-        [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.Conflict)]
         public IActionResult GetCurrentStreak()
         {
             int currentStreak = _statsService.GetCurrentStreak(GuidOfCurrentUser);
 
-            return Ok(currentStreak);
+            return Ok(new { stat = currentStreak });
         }
 
         [HttpGet("number-of-learnt-words")]
-        [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.Conflict)]
         public IActionResult GetNumberOfWordsLearntByUser([FromQuery] StatsQueryFilterNumberOfLearntWordsDto filtersDto)
         {
@@ -72,21 +72,21 @@ namespace Api.Controllers
 
             int numberOfWords = _statsService.GetNumberOfWordsLearntByUser(GuidOfCurrentUser, filters);
 
-            return Ok(numberOfWords);
+            return Ok(new { stat = numberOfWords });
         }
 
         [HttpGet("percent-learnt")]
-        [ProducesResponseType(typeof(double), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.Conflict)]
         public async Task<IActionResult> GetPercentOfWordsLearntByUser()
         {
             double percent = await _statsService.GetPercentOfWordsLearntByUser(GuidOfCurrentUser);
 
-            return Ok(percent);
+            return Ok(new { stat = percent });
         }
 
         [HttpGet("success-rate")]
-        [ProducesResponseType(typeof(double), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.Conflict)]
         public IActionResult GetSuccessRate([FromQuery] StatsQueryFilterSuccessRateDto filtersDto)
         {
@@ -95,7 +95,7 @@ namespace Api.Controllers
 
             double rate = _statsService.GetSuccessRate(filters);
 
-            return Ok(rate);
+            return Ok(new { stat = rate });
         }
     }
 }
